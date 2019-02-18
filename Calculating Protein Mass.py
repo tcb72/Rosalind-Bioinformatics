227d0859ce794ebcd062d37d146ed1433439d3ea
Thomas Biondi <tcb72@cornell.edu>
2019-02-18 14:36:04 -0500

    Create Calculating Protein Mass.py

---
+++ Calculating Protein Mass.py
@@ -0,0 +1,31 @@
+def calcProteinMass(protein_string):
+    # amount of times a protein corresponds to different codons
+    protein_weights = {'A':71.03711,
+                       'C':103.00919,
+                       'D':115.02694,
+                       'E':129.04259,
+                       'F':147.06841,
+                       'G':57.02146,
+                       'H':137.05891,
+                       'I':113.08406,
+                       'K':128.09496,
+                       'L':113.08406,
+                       'M':131.04049,
+                       'N':114.04293,
+                       'P':97.05276,
+                       'Q':128.05858,
+                       'R':156.10111,
+                       'S':87.03203,
+                       'T':101.04768,
+                       'V':99.06841,
+                       'W':186.07931,
+                       'Y':163.06333}
+    # initiate weight as 0
+    weight = 0
+    
+    # get value from protein key, add until end of string
+    for char in protein_string:
+        weight += protein_weights[char]
+    
+    # return weight
+    return(weight)
